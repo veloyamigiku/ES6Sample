f43d7548c8e96b25779dd67949cde57b76bde3e2
xda-yoshioka <xda-yoshioka@kddilabs.jp>
2015-12-18 12:58:01 +0900

    class構文のサンプルソースを追加します。

---
+++ sample0001.js
@@ -0,0 +1,15 @@
+// class構文のサンプルです。
+class Animal {
+  constructor(name, sex) {
+    this.name = name;
+    this.sex = sex;
+  }
+  
+  toString() {
+    return this.name + 'は' + this.sex + 'です。';
+  }
+  
+}
+
+var ani = new Animal('きら', 'メス');
+console.log(ani.toString());
