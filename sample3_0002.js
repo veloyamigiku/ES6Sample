3eadfcf37c6e3c60903f036bd0ae354e8fdbdf19
velo.yamigiku <velo.yamigiku@gmail.com>
2016-01-25 14:14:06 +0900

    連想配列Mapのサンプルを追加します。

---
+++ sample3_0002.js
@@ -0,0 +1,10 @@
+// 連想配列Map
+
+let m = new Map();
+
+m.set('XML', 'eXtensible Markup Language');
+m.set('PHP', 'PHP:Hypertext Preprocessor');
+m.set('JSON', 'Javascript Object Notation');
+
+console.log(m.get('JSON'));
+console.log(m.size);
